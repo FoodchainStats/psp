715a8fe1a668e933eef5a63f105e3b442dc7d988
David Lee <david.lee@defra.gov.uk>
2025-03-25 13:59:06 +0000

    added psp_ac_codes data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Agricultural Accounts items and categories
+#'
+#' @format a dataframe with 3 columns and 100 rows:
+#' \describe{
+#'  \item{ac_id}{an ID from the accounts Final Value Table}
+#'  \item{ac_item}{an item description for an ac_id}
+#'  \item{psp_cat}{an aggregate product category}
+#' }
+"psp_ac_codes"
